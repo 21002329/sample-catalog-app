774d65c668dd56ef3c4300813f990b7a7dc822fa
vagrant <vagrant@vagrant.vm>
2017-11-05 13:32:22 +0000

    Initial commit including the data model.

---
+++ model.py
@@ -0,0 +1,45 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+        """Return object data in serializeable format"""
+        return {
+            'id': self.id,
+            'name': self.name,
+        }
+
+
+class Item(Base):
+    __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    info = Column(String(1000), nullable=False)
+    category_id = Column(Integer, ForeignKey('category.id'), primary_key=True)
+    category = relationship(Category)
+
+    @property
+    def serialize(self):
+        """Return object data in serializeable format"""
+        return {
+            'id': self.id,
+            'name': self.name,
+            'info': self.info,
+        }
+
+
+engine = create_engine('sqlite:///item-catalog.db')
+
+Base.metadata.create_all(engine)
